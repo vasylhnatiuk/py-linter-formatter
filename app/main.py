833ee4450b35468a3741ff3c97138724771db38b
Cергей <38cerg12021979@gmail.com>
2023-12-08 17:33:36 +0200

    08_12_2023_17_33

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 def format_linter_error(error: dict) -> dict:
-    # white your code here
-    pass
+    return {
+        "line": error["line_number"], "column": error["column_number"],
+        "message": error["text"], "name": error["code"], "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # white your code here
-    pass
+    return {
+        "errors": [format_linter_error(index) for index in errors],
+        "path": file_path, "status": "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # white your code here
-    pass
+    return [
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
